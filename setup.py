36caa444fb981d3b4b1ef818453109e934d1e90e
John Hicks <john@tanoti.org>
2019-12-06 10:01:46 +0000

    Fix setup.py so packages are included properly

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 import os
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
-
-# Allow setup.py to be run from any path
-# os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='sphinxcontrib-autosaltsls',
