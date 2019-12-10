04b169e64e90ce48140bb969af7315fe907ae123
John Hicks <john@tanoti.org>
2019-12-10 16:10:12 +0000

    Changed author email

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type='text/x-rst',
     url='https://bitbucket.internal.example.invalid/projects/DEVOPS/repos/ccs-standard-templates/browse',
     author='John Hicks',
-    author_email='john@tanoti.org',
+    author_email='john.p.hicks@gmail.com',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Sphinx :: Extension',
