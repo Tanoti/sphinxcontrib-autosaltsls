f71b5ae0cfba8e22439036309dca20666ee03c03
John Hicks <john@tanoti.org>
2020-01-06 11:50:30 +0000

    Bump version: 0.5.1 → 0.5.2

--- sphinxcontrib/autosaltsls/__init__.py
+++ sphinxcontrib/autosaltsls/__init__.py
@@ -14,7 +14,7 @@ from .mapper import AutoSaltSLSMapper
 
 __author__ = """John Hicks"""
 __email__ = "john@tanoti.org"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 SETTINGS_STRING = [
     "build_dir",
